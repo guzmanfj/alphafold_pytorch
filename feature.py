461a57b3ec119248d0f17265fee8c2c83cff9c2e
Francisco J. Guzman Vega <36437485+guzmanfj@users.noreply.github.com>
2020-03-29 12:48:58 +0300

    Changed exit() for continue so it would generate all .aln files

--- feature.py
+++ feature.py
@@ -161,7 +161,7 @@ def feature_generation(seq_file, out_file):
             aln, aln_id = read_aln(fas_file)
             aln = aln[:, aln[0] != '-']
             write_aln(aln, aln_id, aln_file)
-            exit()
+            continue
 
         if mat_file.exists():
             mat = sio.loadmat(mat_file)
